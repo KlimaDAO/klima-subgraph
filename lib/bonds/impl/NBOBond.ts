ebbad5a579212ddffb73e6e7fcb49fbbadbdebc9
Sekula <sekuloskimilos@gmail.com>
2022-05-14 19:10:19 +0200

    bonds-refactoring: - Added NBO and UBO bonds - Redesigned bond subgraphs in order to have less code that is being repeated

---
+++ lib/bonds/impl/NBOBond.ts
@@ -0,0 +1,58 @@
+import { BigDecimal, BigInt, Address } from "@graphprotocol/graph-ts";
+import { BondV1 } from "../../../bonds/generated/BCTBondV1/BondV1";
+import { ERC20 } from "../../../bonds/generated/BCTBondV1/ERC20";
+import { IBondable } from "../IBondable";
+import { IToken } from "../../tokens/IToken";
+
+import * as constants from "../../utils/Constants";
+import { toDecimal } from "../../utils/Decimals";
+import { NBO } from "../../tokens/impl/NBO";
+
+export class NBOBond implements IBondable {
+  
+  contractAddress: Address;
+
+  constructor(constractAddress: Address) {
+    this.contractAddress = constractAddress;
+  }
+
+  getToken(): IToken {
+    return new NBO();
+  }
+
+  getBondName(): string {
+    return constants.UBO_BOND_TOKEN;
+  }
+
+  getBondFee(): BigDecimal {
+    let bondContract = BondV1.bind(this.contractAddress);
+    let feeRaw = bondContract.terms().value4;
+    const feeDecimal = feeRaw
+      .toBigDecimal()
+      .div(BigDecimal.fromString("10000"));
+
+    return feeDecimal;
+  }
+
+  getBondPrice(priceInUSD: BigInt): BigDecimal {
+    return toDecimal(priceInUSD, 18);
+  }
+
+  getCarbonCustodied(depositAmount: BigInt): BigDecimal {
+    return toDecimal(depositAmount, this.getToken().getDecimals())
+  }
+
+  getTreasuredAmount(): BigDecimal {
+    let treasuryAddress = Address.fromString(constants.TREASURY_ADDRESS);
+
+    let ercContract = ERC20.bind(
+      Address.fromString(this.getToken().getERC20ContractAddress())
+    );
+    let treasuryBalance = toDecimal(
+      ercContract.balanceOf(treasuryAddress),
+      this.getToken().getDecimals()
+    );
+
+    return treasuryBalance;
+  }
+}
---
+++ lib/tokens/impl/KLIMA.ts
@@ -0,0 +1,36 @@
+import { BigDecimal, BigInt, Address, log } from "@graphprotocol/graph-ts";
+import { ERC20 } from '../../../bonds/generated/BCTBondV1/ERC20'
+import { IToken } from "../IToken";
+
+import * as constants from '../../utils/Constants'
+import { toDecimal } from "../../utils/Decimals"
+
+
+export class KLIMA implements IToken {
+
+    contractAddress: string = constants.KLIMA_ERC20_V1_CONTRACT
+    static DECIMALS: number = 18
+
+    getERC20ContractAddress(): string {
+        return this.contractAddress
+    }
+
+    getTokenName(): string {
+        return "KLIMA"
+    }
+    getDecimals(): number {
+        return KLIMA.DECIMALS
+    }
+    getFormattedPrice(rawPrice: BigInt): BigDecimal {
+        return toDecimal(rawPrice, this.getDecimals())
+    }
+    getMarketPrice(): BigDecimal {
+        throw new Error("Method not implemented.");
+    }
+    getTotalSupply(): BigDecimal {
+        let ercContract = ERC20.bind(Address.fromString(this.contractAddress))
+        let totalSupply = toDecimal(ercContract.totalSupply(), this.getDecimals())
+     
+        return totalSupply
+    }
+}

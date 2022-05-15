f8b29c927a7be123ccfd53d5a797302a1f1f5da8
Sekula <sekuloskimilos@gmail.com>
2022-05-15 14:46:06 +0200

    klima-markets: Added DailyPrice and HourlyPrice entities. Currently supporting market and usd price for: KLIMA, BCT, MCO2, NBO, UBO

---
+++ markets/src/PriceHandler.ts
@@ -0,0 +1,32 @@
+import { BigInt, log } from '@graphprotocol/graph-ts';
+import { ERC20, Transfer } from '../generated/BCTPrice/ERC20'
+import { TokenFactory } from '../../lib/tokens/TokenFactory'
+import { dayFromTimestamp, hourFromTimestamp } from '../../lib/utils/Dates'
+
+import { loadOrCreateDailyPrice } from './utils/DailyPrice'
+import { loadOrCreateHourlyPrice } from './utils/HourlyPrice'
+
+export function handleTransfer(event: Transfer): void {
+
+    const token = new TokenFactory().getTokenForAddress(event.address)
+
+    const dayTimestamp = dayFromTimestamp(event.block.timestamp)
+    const dailyId = dayTimestamp + token.getTokenName()
+
+    let dailyPrice = loadOrCreateDailyPrice(dailyId)
+    dailyPrice.token = token.getTokenName()
+    dailyPrice.marketPrice = token.getMarketPrice()
+    dailyPrice.usdPrice = token.getUSDPrice()
+    dailyPrice.timestamp = BigInt.fromString(dayTimestamp)
+    dailyPrice.save()
+
+
+    const hourTimestamp = hourFromTimestamp(event.block.timestamp)
+    const hourlyId = hourTimestamp + token.getTokenName()
+    let hourlyPrice = loadOrCreateHourlyPrice(hourlyId)
+    hourlyPrice.token = token.getTokenName()
+    hourlyPrice.marketPrice = token.getMarketPrice()
+    hourlyPrice.usdPrice = token.getUSDPrice()
+    hourlyPrice.timestamp = BigInt.fromString(hourTimestamp)
+    hourlyPrice.save()
+}

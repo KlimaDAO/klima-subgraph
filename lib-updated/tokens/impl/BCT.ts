d3dc1d49ed455a8fad19c38d674034d648dda7be
Cujowolf <cujowolf12@gmail.com>
2023-04-04 14:43:17 -0500

    Initial Polygon carbon indexing

--- lib-updated/tokens/impl/BCT.ts
+++ lib-updated/tokens/impl/BCT.ts
@@ -9,82 +9,80 @@ import { PriceUtil } from '../../utils/Price'
 import { loadOrCreateToken } from '../../utils/Token'
 
 export class BCT implements IToken {
-  private contractAddress: Address = constants.BCT_ERC20_CONTRACT
-  private klimaToken: KLIMA = new KLIMA()
+    private contractAddress: Address = constants.BCT_ERC20_CONTRACT
+    private klimaToken: KLIMA = new KLIMA()
 
-  getContractAddress(): Address {
-    return this.contractAddress
-  }
-
-  getTokenName(): string {
-    return constants.BCT_TOKEN
-  }
-  getDecimals(): number {
-    return 18
-  }
-
-  getFormattedPrice(rawPrice: BigInt): BigDecimal {
-    return toDecimal(rawPrice, this.getDecimals())
-  }
-
-  getMarketPrice(): BigDecimal {
-    const token = loadOrCreateToken(this.contractAddress)
-    return token.latestPricePerKLIMA
-  }
-
-  getUSDPrice(): BigDecimal {
-    const token = loadOrCreateToken(this.contractAddress)
-    return token.latestPriceUSD
-  }
-
-  updateMarketPrice(timestamp: BigInt, newPrice: BigDecimal = ZERO_BD): void {
-    let token = loadOrCreateToken(this.contractAddress)
-
-    // Set the price directly if we had access to liquidity pool information
-    if (newPrice !== ZERO_BD) {
-      token.latestPricePerKLIMA = newPrice
-      token.latestPricePerKLIMAUpdated = timestamp
-      token.save()
-      return
+    getContractAddress(): Address {
+        return this.contractAddress
     }
 
-    token.latestPricePerKLIMA = PriceUtil.getKLIMA_BCTRate()
-    token.latestPricePerKLIMAUpdated = timestamp
-    token.save()
-  }
-
-  updateUSDPrice(timestamp: BigInt, blockNumber: BigInt, klimaPrice: BigDecimal = ZERO_BD): BigDecimal {
-    let token = loadOrCreateToken(this.contractAddress)
-
-    // We are going through BCT-USD until the liquidity is removed
-    if (blockNumber < constants.BCT_USDC_PAIR_REMOVE_LIQUIDITY_BLOCK) {
-      token.latestPriceUSD = PriceUtil.getBCT_USDRate()
-      token.latestPriceUSDUpdated = timestamp
-      token.save()
-      return token.latestPriceUSD
+    getTokenName(): string {
+        return constants.BCT_TOKEN
+    }
+    getDecimals(): number {
+        return 18
     }
 
-    if (klimaPrice == ZERO_BD) klimaPrice = this.klimaToken.getUSDPrice()
-
-    token.latestPriceUSD = token.latestPricePerKLIMA.times(klimaPrice)
-    token.latestPriceUSDUpdated = timestamp
-    token.save()
-
-    return token.latestPriceUSD
-  }
-
-  getTotalSupply(): BigDecimal {
-    let ercContract = ERC20.bind(this.contractAddress)
-    let totalSupply = toDecimal(ercContract.totalSupply(), this.getDecimals())
-
-    return totalSupply
-  }
-
-  getAddressBalance(address: Address): BigDecimal {
-    const newBalanceRaw = ERC20.bind(this.contractAddress).try_balanceOf(address)
-    if (!newBalanceRaw.reverted) {
-      return toDecimal(newBalanceRaw.value, this.getDecimals())
+    getFormattedPrice(rawPrice: BigInt): BigDecimal {
+        return toDecimal(rawPrice, this.getDecimals())
     }
-    return BigDecimal.fromString('0')
-  }
+
+    getMarketPrice(): BigDecimal {
+        const token = loadOrCreateToken(this.contractAddress)
+        return token.latestPricePerKLIMA
+    }
+
+    getUSDPrice(): BigDecimal {
+        const token = loadOrCreateToken(this.contractAddress)
+        return token.latestPriceUSD
+    }
+
+    updateMarketPrice(timestamp: BigInt, newPrice: BigDecimal = ZERO_BD): void {
+        let token = loadOrCreateToken(this.contractAddress)
+
+        // Set the price directly if we had access to liquidity pool information
+        if (newPrice !== ZERO_BD) {
+            token.latestPricePerKLIMA = newPrice
+            token.latestPricePerKLIMAUpdated = timestamp
+            token.save()
+            return
+        }
+
+        token.latestPricePerKLIMA = PriceUtil.getKLIMA_BCTRate()
+        token.latestPricePerKLIMAUpdated = timestamp
+        token.save()
+    }
+
+    updateUSDPrice(timestamp: BigInt, blockNumber: BigInt, klimaPrice: BigDecimal = ZERO_BD): BigDecimal {
+        let token = loadOrCreateToken(this.contractAddress)
+
+        // We are going through BCT-USD until the liquidity is removed
+        if (blockNumber < constants.BCT_USDC_PAIR_REMOVE_LIQUIDITY_BLOCK) {
+            token.latestPriceUSD = PriceUtil.getBCT_USDRate()
+            token.latestPriceUSDUpdated = timestamp
+            token.save()
+            return token.latestPriceUSD
+        }
+
+        token.latestPriceUSD = token.latestPricePerKLIMA == ZERO_BD ? ZERO_BD : klimaPrice.div(token.latestPricePerKLIMA)
+        token.latestPriceUSDUpdated = timestamp
+        token.save()
+
+        return token.latestPriceUSD
+    }
+
+    getTotalSupply(): BigDecimal {
+        let ercContract = ERC20.bind(this.contractAddress)
+        let totalSupply = toDecimal(ercContract.totalSupply(), this.getDecimals())
+
+        return totalSupply
+    }
+
+    getAddressBalance(address: Address): BigDecimal {
+        const newBalanceRaw = ERC20.bind(this.contractAddress).try_balanceOf(address)
+        if (!newBalanceRaw.reverted) {
+            return toDecimal(newBalanceRaw.value, this.getDecimals())
+        }
+        return BigDecimal.fromString('0')
+    }
 }
---
+++ polygon-carbon/src/TransferHandler.ts
@@ -0,0 +1,5 @@
+import { ZERO_ADDRESS } from '../../lib-updated/utils/Constants'
+import { Transfer } from '../generated/BaseCarbonTonne/ERC20'
+import { loadCarbonOffset } from './utils/CarbonOffset'
+
+export function handleTransfer(event: Transfer): void {}

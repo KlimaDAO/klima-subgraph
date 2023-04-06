1a62a961222c0caa23d5ce2acc7fe9138d69a65b
Cujowolf <cujowolf12@gmail.com>
2023-04-06 15:17:10 -0500

    Add polygon-bridged-carbon

--- lib/utils/Dates.ts
+++ lib/utils/Dates.ts
@@ -1,4 +1,9 @@
-import { BigInt } from "@graphprotocol/graph-ts";
+import { BigInt } from '@graphprotocol/graph-ts'
+
+export function stdYearFromTimestamp(timestamp: BigInt): string {
+    let date = new Date(<i64>timestamp.toI32() * 1000)
+    return date.getUTCFullYear().toString()
+}
 
 export function dayFromTimestamp(timestamp: BigInt): string {
     let day_ts = timestamp.toI32() - (timestamp.toI32() % 86400)

88b68c8559df32cf14110968c20595c3a7be05a3
Cujowolf <cujowolf12@gmail.com>
2023-04-06 14:40:01 -0500

    Update and reset to base repo
    - Update dependency versions across all subgraphs
    - Remove extra subgraphs and return to current deployments

--- polygon-carbon/src/TransferHandler.ts
+++
@@ -1,5 +0,0 @@
-import { ZERO_ADDRESS } from '../../lib-updated/utils/Constants'
-import { Transfer } from '../generated/BaseCarbonTonne/ERC20'
-import { loadCarbonOffset } from './utils/CarbonOffset'
-
-export function handleTransfer(event: Transfer): void {}

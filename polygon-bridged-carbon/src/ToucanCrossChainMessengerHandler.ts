298cf1c93a573d3f07d3a0fb0daa0bb8fd52d596
Cujowolf <cujowolf12@gmail.com>
2023-05-11 15:30:51 -0500

    Add event signatures for v1.1.0 of Toucan bridge

--- polygon-bridged-carbon/src/ToucanCrossChainMessengerHandler.ts
+++ polygon-bridged-carbon/src/ToucanCrossChainMessengerHandler.ts
@@ -1,40 +1,79 @@
 import {
   BridgeRequestReceived,
+  BridgeRequestReceived1 as BridgeRequestReceived_1_1_0,
   BridgeRequestSent,
+  BridgeRequestSent1 as BridgeRequestSent_1_1_0,
 } from '../generated/ToucanCrossChainMessenger/ToucanCrossChainMessenger'
 import { ERC20, Transfer } from '../generated/BaseCarbonTonne/ERC20'
 import { loadOrCreateCarbonOffset } from './utils/CarbonOffsets'
 import { toDecimal } from '../../lib/utils/Decimals'
 import { loadOrCreateTransaction } from './utils/Transactions'
 import { CarbonMetricUtils } from './utils/CarbonMetrics'
-import { Address, BigInt, log } from '@graphprotocol/graph-ts'
+import { Address, BigInt, ethereum, log } from '@graphprotocol/graph-ts'
 import { PoolTokenFactory } from './utils/pool_token/PoolTokenFactory'
 import { loadOrCreateCrosschainBridge } from './utils/CrosschainBridge'
 
-export function handleBridgeRequestReceived(event: BridgeRequestReceived): void {
-  const poolToken = new PoolTokenFactory().getTokenForAddress(event.params.token)
-  let transaction = loadOrCreateTransaction(event.transaction, event.block)
-
-  let crosschainBridge = loadOrCreateCrosschainBridge(event.params.token.toHexString(), transaction)
-  crosschainBridge.value = toDecimal(event.params.amount, 18)
-  crosschainBridge.bridger = event.params.bridger.toHexString()
-  crosschainBridge.direction = 'Received'
-  crosschainBridge.save()
-
-  CarbonMetricUtils.updatePoolTokenSupply(poolToken, event.block.timestamp)
-  CarbonMetricUtils.updateCrosschainTokenSupply(poolToken, event.block.timestamp, event.params.amount.neg())
+export function handleBridgeRequestReceived_1_0_0(event: BridgeRequestReceived): void {
+  processBridgeRequest(
+    'Received',
+    event.params.token,
+    event.transaction,
+    event.params.amount,
+    event.params.bridger,
+    event.block
+  )
 }
 
-export function handleBridgeRequestSent(event: BridgeRequestSent): void {
-  const poolToken = new PoolTokenFactory().getTokenForAddress(event.params.token)
-  let transaction = loadOrCreateTransaction(event.transaction, event.block)
+export function handleBridgeRequestSent_1_0_0(event: BridgeRequestSent): void {
+  processBridgeRequest(
+    'Sent',
+    event.params.token,
+    event.transaction,
+    event.params.amount,
+    event.params.bridger,
+    event.block
+  )
+}
 
-  let crosschainBridge = loadOrCreateCrosschainBridge(event.params.token.toHexString(), transaction)
-  crosschainBridge.value = toDecimal(event.params.amount, poolToken.getDecimals())
-  crosschainBridge.bridger = event.params.bridger.toHexString()
-  crosschainBridge.direction = 'Sent'
+export function handleBridgeRequestReceived_1_1_0(event: BridgeRequestReceived_1_1_0): void {
+  processBridgeRequest(
+    'Received',
+    event.params.token,
+    event.transaction,
+    event.params.amount,
+    event.params.bridger,
+    event.block
+  )
+}
+
+export function handleBridgeRequestSent_1_1_0(event: BridgeRequestSent_1_1_0): void {
+  processBridgeRequest(
+    'Sent',
+    event.params.token,
+    event.transaction,
+    event.params.amount,
+    event.params.bridger,
+    event.block
+  )
+}
+
+function processBridgeRequest(
+  direction: string,
+  token: Address,
+  transaction: ethereum.Transaction,
+  amount: BigInt,
+  bridger: Address,
+  block: ethereum.Block
+): void {
+  const poolToken = new PoolTokenFactory().getTokenForAddress(token)
+  let tx = loadOrCreateTransaction(transaction, block)
+
+  let crosschainBridge = loadOrCreateCrosschainBridge(token.toHexString(), tx)
+  crosschainBridge.value = toDecimal(amount, poolToken.getDecimals())
+  crosschainBridge.bridger = bridger.toHexString()
+  crosschainBridge.direction = direction
   crosschainBridge.save()
 
-  CarbonMetricUtils.updatePoolTokenSupply(poolToken, event.block.timestamp)
-  CarbonMetricUtils.updateCrosschainTokenSupply(poolToken, event.block.timestamp, event.params.amount)
+  CarbonMetricUtils.updatePoolTokenSupply(poolToken, block.timestamp)
+  CarbonMetricUtils.updateCrosschainTokenSupply(poolToken, block.timestamp, amount)
 }

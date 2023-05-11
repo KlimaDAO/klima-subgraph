c46ca2c1bb5efeec51e76412db5ee8ac4f23d3c4
Cujowolf <cujowolf12@gmail.com>
2023-05-11 15:37:28 -0500

    Add v1.1.0 Toucan bridge to Celo subgraph

--- celo-bridged-carbon/src/ToucanCrossChainMessengerHandler.ts
+++ celo-bridged-carbon/src/ToucanCrossChainMessengerHandler.ts
@@ -1,35 +1,76 @@
 import {
   BridgeRequestReceived,
+  BridgeRequestReceived1 as BridgeRequestReceived_1_1_0,
   BridgeRequestSent,
+  BridgeRequestSent1 as BridgeRequestSent_1_1_0,
 } from '../generated/ToucanCrossChainMessenger/ToucanCrossChainMessenger'
 import { toDecimal } from '../../lib/utils/Decimals'
 import { loadOrCreateTransaction } from './utils/Transactions'
 import { CarbonMetricUtils } from './utils/CarbonMetrics'
 import { PoolTokenFactory } from './utils/pool_token/PoolTokenFactory'
 import { loadOrCreateCrosschainBridge } from './utils/CrosschainBridge'
+import { Address, BigInt, ethereum } from '@graphprotocol/graph-ts'
 
-export function handleBridgeRequestReceived(event: BridgeRequestReceived): void {
-  const poolToken = new PoolTokenFactory().getTokenForAddress(event.params.token)
-  let transaction = loadOrCreateTransaction(event.transaction, event.block)
-
-  let crosschainBridge = loadOrCreateCrosschainBridge(event.params.token.toHexString(), transaction)
-  crosschainBridge.value = toDecimal(event.params.amount, 18)
-  crosschainBridge.bridger = event.params.bridger.toHexString()
-  crosschainBridge.direction = 'Received'
-  crosschainBridge.save()
-
-  CarbonMetricUtils.updatePoolTokenSupply(poolToken, event.block.timestamp)
+export function handleBridgeRequestReceived_1_0_0(event: BridgeRequestReceived): void {
+  processBridgeRequest(
+    'Received',
+    event.params.token,
+    event.transaction,
+    event.params.amount,
+    event.params.bridger,
+    event.block
+  )
 }
 
-export function handleBridgeRequestSent(event: BridgeRequestSent): void {
-  const poolToken = new PoolTokenFactory().getTokenForAddress(event.params.token)
-  let transaction = loadOrCreateTransaction(event.transaction, event.block)
+export function handleBridgeRequestSent_1_0_0(event: BridgeRequestSent): void {
+  processBridgeRequest(
+    'Sent',
+    event.params.token,
+    event.transaction,
+    event.params.amount,
+    event.params.bridger,
+    event.block
+  )
+}
 
-  let crosschainBridge = loadOrCreateCrosschainBridge(event.params.token.toHexString(), transaction)
-  crosschainBridge.value = toDecimal(event.params.amount, 18)
-  crosschainBridge.bridger = event.params.bridger.toHexString()
-  crosschainBridge.direction = 'Sent'
+export function handleBridgeRequestReceived_1_1_0(event: BridgeRequestReceived_1_1_0): void {
+  processBridgeRequest(
+    'Received',
+    event.params.token,
+    event.transaction,
+    event.params.amount,
+    event.params.bridger,
+    event.block
+  )
+}
+
+export function handleBridgeRequestSent_1_1_0(event: BridgeRequestSent_1_1_0): void {
+  processBridgeRequest(
+    'Sent',
+    event.params.token,
+    event.transaction,
+    event.params.amount,
+    event.params.bridger,
+    event.block
+  )
+}
+
+function processBridgeRequest(
+  direction: string,
+  token: Address,
+  transaction: ethereum.Transaction,
+  amount: BigInt,
+  bridger: Address,
+  block: ethereum.Block
+): void {
+  const poolToken = new PoolTokenFactory().getTokenForAddress(token)
+  let tx = loadOrCreateTransaction(transaction, block)
+
+  let crosschainBridge = loadOrCreateCrosschainBridge(token.toHexString(), tx)
+  crosschainBridge.value = toDecimal(amount, 18)
+  crosschainBridge.bridger = bridger.toHexString()
+  crosschainBridge.direction = direction
   crosschainBridge.save()
 
-  CarbonMetricUtils.updatePoolTokenSupply(poolToken, event.block.timestamp)
+  CarbonMetricUtils.updatePoolTokenSupply(poolToken, block.timestamp)
 }

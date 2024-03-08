8f4b983723ffcaa6ebac52bd0e60d6a949e85e7d
psparacino <tufnelenterprises@gmail.com>
2024-03-08 12:46:03 -0500

    working add mumbai deployment

--- carbonmark/src/ProjectManager.ts
+++ carbonmark/src/ProjectManager.ts
@@ -1,11 +1,33 @@
-import { log } from '@graphprotocol/graph-ts'
+import { log, Bytes, BigInt } from '@graphprotocol/graph-ts'
 import {
   ProjectAdded as ProjectAddedEvent,
   ProjectRemoved as ProjectRemovedEvent,
 } from '../generated/ProjectManager/ProjectManager'
+import {Project } from '../generated/schema'
+import { createCategory, createCountry } from './Entities'
 
 export function handleProjectAdded(event: ProjectAddedEvent): void {
-  log.info('fuck yeah', [])
+  const projectAddress = event.params.id.toHexString()
+  let project = Project.load(projectAddress)
+
+  const registry = event.params.projectId.split('-')[0]
+
+  if (project == null) {
+    project = new Project(projectAddress)
+    project.key = event.params.projectId
+    project.name = event.params.name
+    project.methodology = event.params.methodologies[0]
+    project.vintage = BigInt.fromString(event.params.vintages[0])
+    project.projectAddress = Bytes.fromHexString(projectAddress)
+    project.registry = registry
+    project.category = event.params.category
+    project.country = event.params.country
+    project.save()
+
+    createCountry(project.country)
+    createCategory(project.category)
+  }
+
 }
 
 export function handleProjectRemoved(event: ProjectRemovedEvent): void {

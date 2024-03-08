44453a00612a7495d444c7d708b478d72331beeb
psparacino <tufnelenterprises@gmail.com>
2024-03-08 17:09:19 -0500

    fix vintages on handleProjectAdded

--- carbonmark/src/ProjectManager.ts
+++ carbonmark/src/ProjectManager.ts
@@ -1,4 +1,4 @@
-import { log, Bytes, BigInt, store } from '@graphprotocol/graph-ts'
+import { log, Address, BigInt, store } from '@graphprotocol/graph-ts'
 import {
   ProjectAdded as ProjectAddedEvent,
   ProjectRemoved as ProjectRemovedEvent,
@@ -8,25 +8,27 @@ import { createCategory, createCountry } from './Entities'
 
 export function handleProjectAdded(event: ProjectAddedEvent): void {
   log.info('handleProjectAdded fired {}', [event.params.id.toHexString()])
-  const projectAddress = event.params.id.toHexString()
-  let project = Project.load(projectAddress)
 
   const registry = event.params.projectId.split('-')[0]
 
-  if (project == null) {
-    project = new Project(projectAddress)
-    project.key = event.params.projectId
-    project.name = event.params.name
-    project.methodology = event.params.methodologies[0]
-    project.vintage = BigInt.fromString(event.params.vintages[0])
-    project.projectAddress = Bytes.fromHexString(projectAddress)
-    project.registry = registry
-    project.category = event.params.category
-    project.country = event.params.country
-    project.save()
+  for (let i = 0; i < event.params.vintages.length; i++) {
+    const id = event.params.projectId + '-' + event.params.vintages[i].toString()
+    let project = Project.load(id)
+    if (project == null) {
+      project = new Project(id)
+      project.key = event.params.projectId // maybe change this on the contract for better naming
+      project.name = event.params.name
+      project.methodology = event.params.methodologies[0]
+      project.vintage = BigInt.fromString(event.params.vintages[i])
+      project.projectAddress = event.params.id // rename in contract here as well
+      project.registry = registry
+      project.category = event.params.category
+      project.country = event.params.country
+      project.save()
 
-    createCountry(project.country)
-    createCategory(project.category)
+      createCountry(project.country)
+      createCategory(project.category)
+    }
   }
 }
 

a22d5edc4a8d4580a99bb9c078c99ed305f594c4
psparacino <tufnelenterprises@gmail.com>
2024-03-08 12:46:03 -0500

    working add mumbai deployment

--- lib/projects/addProject.ts
+++ lib/projects/addProject.ts
@@ -11,17 +11,31 @@ const projectInfoFacetAddress = addresses.projectManager.address
 
 const projectManagerContract = getContract(projectInfoFacetAddress, ProjectManagerAbi)
 
-const updateProjectInfo = async () => {
+const addProject = async () => {
   // upload and pin the project info to IPFS
 
-  // call setter on facet
+  const projectData = {
+    id: '0x000000000000000000000000000000000000DEAD',
+    projectId: 'TestItAll',
+    vintages: ['2021', '2022'],
+    name: 'Fake Project for Testing',
+    methodologies: ['Method1', 'Method2'],
+    category: 'Positive Vibes',
+    country: 'Jupiter',
+    description: 'fakefakefake123',
+  }
+
+  // call to emit ProjectAdded event
   try {
-    const tx = await projectManagerContract.addProject()
+    const tx = await projectManagerContract.addProject(
+      projectData
+    )
     const receipt = await provider.waitForTransaction(tx.hash)
     console.log('project info updated', receipt.transactionHash)
   } catch (e) {
-    console.error('error updating project info', e.error.error.error.message)
+    // console.error('error updating project info', e.error.error.error.message)
+    console.error('error updating project info', e)
   }
 }
 
-updateProjectInfo()
+addProject()

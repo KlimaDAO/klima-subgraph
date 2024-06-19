a395bec5e53be884843d2eb95d72bf0ff89bee8d
psparacino <tufnelenterprises@gmail.com>
2024-06-19 10:30:05 -0400

    requestId helper function

--- polygon-digital-carbon/utils/getRetirementsContractAddress.ts
+++ polygon-digital-carbon/utils/getRetirementsContractAddress.ts
@@ -1,6 +1,10 @@
 import { AMOY_KLIMA_CARBON_RETIREMENTS_CONTRACT, KLIMA_CARBON_RETIREMENTS_CONTRACT } from '../../lib/utils/Constants'
-import { Address } from '@graphprotocol/graph-ts'
+import { Address, Bytes, ByteArray, BigInt } from '@graphprotocol/graph-ts'
 
 export function getRetirementsContractAddress(network: string): Address {
   return network == 'matic' ? KLIMA_CARBON_RETIREMENTS_CONTRACT : AMOY_KLIMA_CARBON_RETIREMENTS_CONTRACT
+}
+
+export function getC3OffsetRequestId(fromToken: Address, beneficiaryAddress: Address, index: BigInt): string {
+  return fromToken.toHexString() + '-' + beneficiaryAddress.toHexString() + '-' + index.toString()
 }

8bcc88409b933f1553c692f2e251e51f0fc540f4
psparacino <tufnelenterprises@gmail.com>
2024-06-25 13:53:42 -0400

    revise enum for assemblyscript

--- polygon-digital-carbon/utils/enums.ts
+++ polygon-digital-carbon/utils/enums.ts
@@ -1,7 +1,6 @@
-export enum BridgeStatus {
-    FINALIZED = 'FINALIZED',
-    REQUESTED = 'REQUESTED',
-    REVERTED = 'REVERTED',
-    AWAITING = 'AWAITING'
-  }
-  
+export class BridgeStatus {
+    static readonly FINALIZED: string = 'FINALIZED';
+    static readonly REQUESTED: string = 'REQUESTED';
+    static readonly REVERTED: string = 'REVERTED';
+    static readonly AWAITING: string = 'AWAITING';
+}

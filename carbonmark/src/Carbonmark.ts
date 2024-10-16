6f7f2779ff83311d11366c4d8afb1ebfa77212c7
psparacino <tufnelenterprises@gmail.com>
2024-10-16 12:06:22 -0400

    testing commit 1.0.16

--- carbonmark/src/Carbonmark.ts
+++ carbonmark/src/Carbonmark.ts
@@ -193,7 +193,7 @@ export function handleListingFilled(event: ListingFilled): void {
   purchase.listing = listing.id
   purchase.save()
 }
-// random comment. v1.0.15
+// random comment. v1.0.16
 export function handleListingCancelled(event: ListingCancelled): void {
   let listing = loadOrCreateListing(event.params.id.toHexString())
 

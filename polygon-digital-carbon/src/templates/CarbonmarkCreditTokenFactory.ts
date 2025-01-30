fe27433f0a581c77f0175b3962de1b895e052bfa
biwano <biwano@gmail.com>
2025-01-30 19:24:07 +0100

    Full CMARK Credits support (#331)
    * Test mode + abi fixes
    
    * Remove test mode
    
    * handleNewCarbonmarkCredit code fixes
    
    * Matchstick linux workaround
    
    * Add generated test files to git ignore
    
    * Test token creation
    
    * Test created credit
    
    * Test created project
    
    * Cleanup
    
    * Add token transfer tests
    
    * Self review
    
    * Handle retirements
    
    * Retirements with CMARK token
    
    * Self review
    
    * Take into account prepare commands
    
    * CMARK projects data update
    
    * Simplify scripts
    
    * Fix type compile error
    
    * Bump carbonmark version
    
    * Restore yarn.lock
    
    * Fix type
    
    * Fix type
    
    * Fix types
    
    * Force runner ubuntu-22.04
    
    * Put back the comment about the cancel event
    
    * Fix Projects.ts
    
    * Remove handleCarbonmarkCreditTransfer handler
    
    * Fix local network config
    
    * Fix network config for local environment
    
    * Makefile commands for CMARK
    
    * Fixes
    
    * Fixes
    
    * Better import of .env for makefile
    
    * Fix cmark retirement handler
    
    * update yarn

--- polygon-digital-carbon/src/templates/CarbonmarkCreditTokenFactory.ts
+++ polygon-digital-carbon/src/templates/CarbonmarkCreditTokenFactory.ts
@@ -1,16 +1,14 @@
 import { CarbonmarkCreditToken } from '../../generated/templates'
-import {
-  NewTokenProject
-} from '../../generated/C3ProjectTokenFactory/C3ProjectTokenFactory'
 import { loadOrCreateCarbonCredit, updateCarbonCreditWithCall } from '../utils/CarbonCredit'
 import { createTokenWithCall } from '../utils/Token'
+import { Issued } from '../../generated/CarbonmarkCreditTokenFactory/CarbonmarkCreditTokenFactory'
 
 
-export function handleNewCarbonmarkCredit(event: NewTokenProject): void {
-  // Start indexing the C3T tokens; `event.params.tokenAddress` is the
+export function handleNewCarbonmarkCredit(event: Issued): void {
+  // Start indexing the Carbonmak tokens; `event.params.tokenAddress` is the
   // address of the new token contract
-  CarbonmarkCreditToken.create(event.params.tokenAddress)
-  loadOrCreateCarbonCredit(event.params.tokenAddress, 'CMARK', null)
-  createTokenWithCall(event.params.tokenAddress, event.block)
-  updateCarbonCreditWithCall(event.params.tokenAddress, '')
+  CarbonmarkCreditToken.create(event.params.creditTokenAddress)
+  loadOrCreateCarbonCredit(event.params.creditTokenAddress, 'CMARK', null)
+  createTokenWithCall(event.params.creditTokenAddress, event.block)
+  updateCarbonCreditWithCall(event.params.creditTokenAddress, '')
 }

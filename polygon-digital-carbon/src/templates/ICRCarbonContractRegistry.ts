5b359facb3e421fa2b11724b5b3014fef8d9f2e3
psparacino <tufnelenterprises@gmail.com>
2025-07-09 12:05:24 -0400

    ExPostCreated: contract call to retrieve name if unset

--- polygon-digital-carbon/src/templates/ICRCarbonContractRegistry.ts
+++ polygon-digital-carbon/src/templates/ICRCarbonContractRegistry.ts
@@ -1,22 +1,12 @@
 import { ICRProjectContract } from '../../generated/templates'
 import { ProjectCreated } from '../../generated/ICRCarbonContractRegistry/ICRCarbonContractRegistry'
 import { log } from '@graphprotocol/graph-ts'
-import { ICRProjectIntermediate } from '../../generated/schema'
 
 export function handleNewICC(event: ProjectCreated): void {
   // Start indexing the C3T tokens; `event.params.tokenAddress` is the
   // address of the new token contract
 
   ICRProjectContract.create(event.params.projectAddress)
-
-  let project = ICRProjectIntermediate.load(event.params.projectAddress)
-
-  if (project == null) {
-    project = new ICRProjectIntermediate(event.params.projectAddress)
-  }
-
-  project.projectName = event.params.projectName
-  project.save()
 
   log.info('Created new ICR Project Datasource', [])
 }

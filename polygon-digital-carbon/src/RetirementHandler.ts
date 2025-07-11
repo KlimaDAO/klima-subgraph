d36785537aca0dff7c17c5f09e6a8012f8a2ec42
psparacino <tufnelenterprises@gmail.com>
2025-07-11 15:37:35 -0400

    remove testing variables

--- polygon-digital-carbon/src/RetirementHandler.ts
+++ polygon-digital-carbon/src/RetirementHandler.ts
@@ -36,7 +36,6 @@ import { C3RetirementMetadata as C3RetirementMetadataTemplate } from '../generat
 import { extractIpfsHash } from '../utils/ipfs'
 import { returnedPoccID } from '../generated/Coorest/Coorest'
 
-const testCounter = 1;
 
 export function saveToucanRetirement(event: Retired): void {
   // Disregard events with zero amount
